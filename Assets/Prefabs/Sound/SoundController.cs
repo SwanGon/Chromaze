cb129715f47158e3c29e179ecc2c941608422f25
Thysma23 <thysma2003@gmail.com>
2024-12-14 13:38:13 +0100

    feat: :sparkles: add soundManager

---
+++ Assets/Prefabs/Sound/SoundController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class SoundController : MonoBehaviour
+{
+    private Dictionary<string, AudioSource> colorAudioMap;
+
+    public static SoundController Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+        colorAudioMap = new Dictionary<string, AudioSource>();
+        var audioSources = GetComponents<AudioSource>();
+        colorAudioMap.Add("Red", audioSources[0]);
+        colorAudioMap.Add("Blue", audioSources[1]);
+
+        StartMusic();
+    }
+
+    private void StartMusic()
+    {
+        foreach (AudioSource audioSource in GetComponents<AudioSource>())
+        {
+            audioSource.Play();
+        }
+    }
+
+    public void stopMusic(string color)
+    {
+        
+        colorAudioMap[color].mute = true;    
+    }
+
+    public void playMusic(string color)
+    {
+        colorAudioMap[color].mute = false;
+    }
+}
