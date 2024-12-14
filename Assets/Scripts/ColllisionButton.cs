4ce654f29132b56125fd9c06a594d0e8284a4a34
MeguminFireFox <ambrehardy013@gmail.com>
2024-12-14 14:21:27 +0100

    feat: :sparkles: Update code collision

--- Assets/Scripts/ColllisionButton.cs
+++ Assets/Scripts/ColllisionButton.cs
@@ -1,13 +1,36 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
 
 public class ColllisionButton : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rb;
+    [SerializeField] private TilemapCollider2D _collider;
+    [SerializeField] private List<GameObject> _objectList;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            _objectList.Add(collision.gameObject);
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _objectList.RemoveAt(1);
+        }
+    }
+
+    private void Update()
+    {
+        if (_objectList.Count != 0)
+        {
+            _collider.isTrigger = true;
+        }
+        else
+        {
+            _collider.isTrigger = false;
         }
     }
 }
