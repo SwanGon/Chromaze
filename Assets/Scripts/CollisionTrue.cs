d818b1eea6880476b3afe29ca9e9c3a4d3b24add
MeguminFireFox <ambrehardy013@gmail.com>
2024-12-15 11:42:03 +0100

    feat: :sparkles: Implemant the muret

--- Assets/Scripts/CollisionTrue.cs
+++ Assets/Scripts/CollisionTrue.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CollisionTrue : MonoBehaviour
 {
-    [SerializeField] private GameObject _gameObject;
+    [SerializeField] private TilemapCollider2D _gameObject;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            _gameObject.SetActive(true);
+            _gameObject.isTrigger = false;
         }
     }
 }
